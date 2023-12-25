3c5feecfd270f53d8a98707a6fe53e6776876c3d
Doniyorbek Ergashev <doniyorbek3322@mail.ru>
2023-12-25 10:54:08 +0500

    two

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const tokenModel = require("../models/token.model");
 const otpModel = require("../models/otp.model");
 const userModel = require("../models/user.model");
 const crypto = require("crypto");
-const bcript = require("bcrypt")
+const bcrypt = require("bcrypt")
 const router = require("express").Router();
 const sendEmail = require("../utils/sendEmail");
 const { checkToken } = require("../middlewares/authMiddleware");
@@ -16,8 +16,8 @@ router.post("/signup", async (req, res) => {
         const otpCode = generateOTP();
         const otp = new otpModel({ phone_number: phone_number, otp: otpCode });
 
-        const salt = await bcript.genSalt(10);
-        otp.otp = await bcript.hash(otp.otp, salt);
+        const salt = await bcrypt.genSalt(10);
+        otp.otp = await bcrypt.hash(otp.otp, salt);
 
         const otpResult = await otp.save();
 
@@ -42,7 +42,7 @@ router.post("/signup/verify", async (req, res) => {
         if (otpHoder.length == 0) return res.status(400).json("You use an Expired OTP!");
         const lastOtpFind = otpHoder[otpHoder.length - 1];
 
-        const validUser = await bcript.compare(otp, lastOtpFind.otp);
+        const validUser = await bcrypt.compare(otp, lastOtpFind.otp);
 
         if (lastOtpFind.phone_number === phone_number && validUser) {
             let user = await userModel.findOne({ phone_number: phone_number });
@@ -89,228 +89,228 @@ router.get("/user/:id", checkToken, async (req, res) => {
 });
 
 
-router.post("/register", async (req, res) => {
-    try {
+// router.post("/register", async (req, res) => {
+//     try {
 
-        let user = await userModel.findOne({ email: req.body.email });
-        if (user) return res.status(400).json(`${req.body.email} allaqachon ro'yxatdan o'tgan!`);
+//         let user = await userModel.findOne({ email: req.body.email });
+//         if (user) return res.status(400).json(`${req.body.email} allaqachon ro'yxatdan o'tgan!`);
 
-        user = await new userModel(req.body).save();
+//         user = await new userModel(req.body).save();
 
-        const token = await new tokenModel({
-            userId: user._id,
-            token: crypto.randomBytes(32).toString("hex"),
-        }).save();
-        let message = {
-            email: user.email,
-            subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
-            html: `<a href="${process.env.BASE_URL}/users/${user._id}/verify/${token.token}">Tasdiqlash</a>`
-        }
+//         const token = await new tokenModel({
+//             userId: user._id,
+//             token: crypto.randomBytes(32).toString("hex"),
+//         }).save();
+//         let message = {
+//             email: user.email,
+//             subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
+//             html: `<a href="${process.env.BASE_URL}/users/${user._id}/verify/${token.token}">Tasdiqlash</a>`
+//         }
 
-        const successjsonEmail = await sendEmail(message);
-        if (!successjsonEmail) {
-            await userModel.findByIdAndDelete(user._id);
-            await tokenModel.findByIdAndDelete(token._id);
-            return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!")
-        }
+//         const successjsonEmail = await sendEmail(message);
+//         if (!successjsonEmail) {
+//             await userModel.findByIdAndDelete(user._id);
+//             await tokenModel.findByIdAndDelete(token._id);
+//             return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!")
+//         }
 
-        return res.status(200).json({ message: "Biz sizning pochtangizga tasdiqlovchi havola yubordik iltimos tasdiqlang !" })
+//         return res.status(200).json({ message: "Biz sizning pochtangizga tasdiqlovchi havola yubordik iltimos tasdiqlang !" })
 
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
-
-
-router.get("/users/:id/verify/:token", async (req, res) => {
-    try {
-        const user = await userModel.findOne({ _id: req.params.id });
-        if (!user) return res.status(400).json("user_id Yaroqsiz");
-        const token = await tokenModel.findOne({ token: req.params.token });
-        if (!token) return res.status(400).json("Token Yaroqsiz!");
-        await userModel.updateOne({ _id: user._id, verified: true });
-        const delToken = await tokenModel.findByIdAndDelete(token._id);
-
-        if (delToken) return res.json({ message: "Elektron pochta muvaffaqiyatli tasdiqlandi" });
-
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
 
 
+// router.get("/users/:id/verify/:token", async (req, res) => {
+//     try {
+//         const user = await userModel.findOne({ _id: req.params.id });
+//         if (!user) return res.status(400).json("user_id Yaroqsiz");
+//         const token = await tokenModel.findOne({ token: req.params.token });
+//         if (!token) return res.status(400).json("Token Yaroqsiz!");
+//         await userModel.updateOne({ _id: user._id, verified: true });
+//         const delToken = await tokenModel.findByIdAndDelete(token._id);
 
-router.post("/auth", async (req, res) => {
-    try {
-        const user = await userModel.findOne({ email: req.body.email });
-        if (!user) return res.status(404).json("Yaroqsiz elektron pochta");
-        const validPassword = await user.comparePassword(req.body.password);
-        if (!validPassword) return res.status(404).json("Yaroqsiz Parol");
-        if (!user.verified) {
-            let token = await tokenModel.findOne({ userId: user._id });
-            if (!token) {
-                token = await new tokenModel({
-                    userId: user._id,
-                    token: crypto.randomBytes(32).toString('hex')
-                }).save();
+//         if (delToken) return res.json({ message: "Elektron pochta muvaffaqiyatli tasdiqlandi" });
 
-                let message = {
-                    email: user.email,
-                    subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
-                    html: `<a href="${process.env.BASE_URL}/users/${user._id}/verify/${token.token}">Tasdiqlash</a>`
-                }
-
-                const successjsonEmail = await jsonEmail(message);
-                console.log("salom")
-                if (!successjsonEmail) return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!");
-            }
-
-            return res.status(200).json({ message: "Biz sizning pochtangizga tasdiqlovchi havola yubordik iltimos tasdiqlang !" })
-
-        }
-
-        const token = await user.generateToken();
-        res.json({
-            user: {
-                _id: user._id,
-                firstname: user.firstname,
-                lastname: user.lastname,
-                email: user.email,
-                token,
-            },
-
-            message: "logged in successfully"
-        });
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
 
 
 
-router.post("/reset-password", async (req, res) => {
-    try {
-        let user = await userModel.findOne({ email: req.body.email });
-        if (!user) return res.status(404).json("Bergan elektron pochta manzili mavjud emas!");
-        let token = await tokenModel.findOne({ userId: user._id })
-        console.log(token)
-        if (!token) {
-            token = await tokenModel({
-                userId: user._id,
-                token: crypto.randomBytes(32).toString('hex')
-            }).save();
-        }
+// router.post("/auth", async (req, res) => {
+//     try {
+//         const user = await userModel.findOne({ email: req.body.email });
+//         if (!user) return res.status(404).json("Yaroqsiz elektron pochta");
+//         const validPassword = await user.comparePassword(req.body.password);
+//         if (!validPassword) return res.status(404).json("Yaroqsiz Parol");
+//         if (!user.verified) {
+//             let token = await tokenModel.findOne({ userId: user._id });
+//             if (!token) {
+//                 token = await new tokenModel({
+//                     userId: user._id,
+//                     token: crypto.randomBytes(32).toString('hex')
+//                 }).save();
 
-        let message = {
-            email: user.email,
-            subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
-            html: `${process.env.BASE_URL}/reset-password/${user._id}/${token.token}`
-        }
+//                 let message = {
+//                     email: user.email,
+//                     subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
+//                     html: `<a href="${process.env.BASE_URL}/users/${user._id}/verify/${token.token}">Tasdiqlash</a>`
+//                 }
 
-        const successjsonEmail = sendEmail(message);
-        if (!successjsonEmail) return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!");
-        return res.json({ message: "Parolni tiklash havolasi elektron pochta hisobingizga yuborildi" })
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
+//                 const successjsonEmail = await jsonEmail(message);
+//                 console.log("salom")
+//                 if (!successjsonEmail) return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!");
+//             }
 
+//             return res.status(200).json({ message: "Biz sizning pochtangizga tasdiqlovchi havola yubordik iltimos tasdiqlang !" })
 
-router.get("/reset-password/:id/:token", async (req, res) => {
-    try {
-        const user = await userModel.findById(req.params.id);
-        if (!user) return res.status(404).json({ message: "Havoladagi userId haqiqiy emas!" });
-        const token = await tokenModel.findOne({ userId: user._id, token: req.params.token });
-        if (!token) return res.status(404).json({ message: "Havoladagi token haqiqiy emas!" });
-        res.status(200).json({ message: "Muoffaqqiyatli parol o'zgartish imkoniyati " })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.message);
-    }
-});
+//         }
 
+//         const token = await user.generateToken();
+//         res.json({
+//             user: {
+//                 _id: user._id,
+//                 firstname: user.firstname,
+//                 lastname: user.lastname,
+//                 email: user.email,
+//                 token,
+//             },
 
-
-//  set new password
-router.post("/reset-password/:id/:token", async (req, res) => {
-    try {
-
-        const user = await userModel.findOne({ _id: req.params.id });
-        if (!user) return res.status(404).json({ message: "Yaroqsiz user id" });
-
-        const token = await tokenModel.findOne({
-            userId: user._id,
-            token: req.params.token,
-        });
-        if (!token) return res.status(404).json({ message: "Yaroqsiz token" });
-
-        if (!user.verified) user.verified = true;
-        user.password = req.body.password;
-        await user.save();
-        await token.deleteOne();
-        return res.status(200).json({ message: "Parol muvaffaqiyatli tiklandi", user });
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
+//             message: "logged in successfully"
+//         });
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
 
 
 
+// router.post("/reset-password", async (req, res) => {
+//     try {
+//         let user = await userModel.findOne({ email: req.body.email });
+//         if (!user) return res.status(404).json("Bergan elektron pochta manzili mavjud emas!");
+//         let token = await tokenModel.findOne({ userId: user._id })
+//         console.log(token)
+//         if (!token) {
+//             token = await tokenModel({
+//                 userId: user._id,
+//                 token: crypto.randomBytes(32).toString('hex')
+//             }).save();
+//         }
 
-router.get("/users", async (req, res) => {
-    try {
-        const users = await userModel.find();
-        res.json(users)
-    } catch (error) {
+//         let message = {
+//             email: user.email,
+//             subject: "Ro'yxatdan o'tish uchun pastdagi Tasdiqlash havolasini usitiga bosing 👇",
+//             html: `${process.env.BASE_URL}/reset-password/${user._id}/${token.token}`
+//         }
 
-    }
-});
-
-
-router.post("/reset-password/:id/:token", async (req, res) => {
-    try {
-        const user = await userModel.findOne({ _id: req.params.id });
-        if (!user) return res.status(404).json({ message: "user id noto'g'ri" });
-        let token = await tokenModel.findOne({ userId: user._id, token: req.params.token });
-        if (!token) return res.status(404).json("token xato");
-        if (!user.verified) user.verified = true;
-        const hashPassword = user.comparePassword(req.body.password);
-        user.password = hashPassword;
-        await token.remove();
-        await user.save();
-        res.status(200).json({ message: "Parol muvaffaqiyatli tiklandi" });
-
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-});
-
-router.get("/user/:id", async (req, res) => {
-    try {
-        const user = await userModel.findById(req.params.id)
-            .populate({
-                path: "shops",
-                populate: {
-                    path: "products"
-                }
-            })
-
-        res.json(user)
-    } catch (error) {
-
-    }
-});
+//         const successjsonEmail = sendEmail(message);
+//         if (!successjsonEmail) return res.status(400).json("Elektron pochtangizga tasdiqlash xabarini yuborib bo'lmadi Elektron pochtangizni tekshiring!");
+//         return res.json({ message: "Parolni tiklash havolasi elektron pochta hisobingizga yuborildi" })
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
 
 
-router.put("/user/:id", async (req, res) => {
-    try {
-        console.log(req.body)
-        const result = await userModel.findByIdAndUpdate(req.params.id, req.body);
-        return res.status(200).json({ message: "Muoffaqqiyatli yangilanish", result })
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-});
+// router.get("/reset-password/:id/:token", async (req, res) => {
+//     try {
+//         const user = await userModel.findById(req.params.id);
+//         if (!user) return res.status(404).json({ message: "Havoladagi userId haqiqiy emas!" });
+//         const token = await tokenModel.findOne({ userId: user._id, token: req.params.token });
+//         if (!token) return res.status(404).json({ message: "Havoladagi token haqiqiy emas!" });
+//         res.status(200).json({ message: "Muoffaqqiyatli parol o'zgartish imkoniyati " })
+//     } catch (error) {
+//         console.log(error)
+//         return res.status(500).json(error.message);
+//     }
+// });
+
+
+
+// //  set new password
+// router.post("/reset-password/:id/:token", async (req, res) => {
+//     try {
+
+//         const user = await userModel.findOne({ _id: req.params.id });
+//         if (!user) return res.status(404).json({ message: "Yaroqsiz user id" });
+
+//         const token = await tokenModel.findOne({
+//             userId: user._id,
+//             token: req.params.token,
+//         });
+//         if (!token) return res.status(404).json({ message: "Yaroqsiz token" });
+
+//         if (!user.verified) user.verified = true;
+//         user.password = req.body.password;
+//         await user.save();
+//         await token.deleteOne();
+//         return res.status(200).json({ message: "Parol muvaffaqiyatli tiklandi", user });
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
+
+
+
+
+// router.get("/users", async (req, res) => {
+//     try {
+//         const users = await userModel.find();
+//         res.json(users)
+//     } catch (error) {
+
+//     }
+// });
+
+
+// router.post("/reset-password/:id/:token", async (req, res) => {
+//     try {
+//         const user = await userModel.findOne({ _id: req.params.id });
+//         if (!user) return res.status(404).json({ message: "user id noto'g'ri" });
+//         let token = await tokenModel.findOne({ userId: user._id, token: req.params.token });
+//         if (!token) return res.status(404).json("token xato");
+//         if (!user.verified) user.verified = true;
+//         const hashPassword = user.comparePassword(req.body.password);
+//         user.password = hashPassword;
+//         await token.remove();
+//         await user.save();
+//         res.status(200).json({ message: "Parol muvaffaqiyatli tiklandi" });
+
+//     } catch (error) {
+//         res.status(500).json(error.message);
+//     }
+// });
+
+// router.get("/user/:id", async (req, res) => {
+//     try {
+//         const user = await userModel.findById(req.params.id)
+//             .populate({
+//                 path: "shops",
+//                 populate: {
+//                     path: "products"
+//                 }
+//             })
+
+//         res.json(user)
+//     } catch (error) {
+
+//     }
+// });
+
+
+// router.put("/user/:id", async (req, res) => {
+//     try {
+//         console.log(req.body)
+//         const result = await userModel.findByIdAndUpdate(req.params.id, req.body);
+//         return res.status(200).json({ message: "Muoffaqqiyatli yangilanish", result })
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// });
 
 module.exports = router;
 
