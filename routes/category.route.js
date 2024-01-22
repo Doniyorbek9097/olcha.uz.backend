f2845e798a8963d0d103f3e0ef7cc1b443174c28
Doniyorbek Ergashev <doniyorbek3322@mail.ru>
2024-01-22 13:19:31 +0500

    yangi

--- routes/category.route.js
+++ routes/category.route.js
@@ -115,6 +115,23 @@ router.get("/category", async (req, res) => {
 
 
 // Get byId Category 
+router.get("/category/:id", async (req, res) => {
+    try {
+        const category = await categoryModel.findById(req.params.id);
+        if(!category) return res.status(404).send("Category topilmadi");
+        return res.status(200).json(category);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error.message)
+    }
+})
+
+
+
+
+
+// Get by slug name 
 router.get("/category/:slug", async (req, res) => {
     try {
         const lang = req.headers["lang"];
