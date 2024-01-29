0688634658e928053a70abcc2ac88a66b3e5ce99
Doniyorbek Ergashev <doniyorbek3322@mail.ru>
2024-01-30 03:24:48 +0500

    yanigilanish

--- utils/base64ToFile.js
+++ utils/base64ToFile.js
@@ -78,12 +78,12 @@ class Base64ToFile {
         filePath,
         Buffer.from(base64Image, 'base64'), 
         (err) => {
-            if(err) rejact(err);
+            if(err) return rejact(err);
             sharp(filePath)
             .resize({ width: 800 })
             .toFormat('webp') 
             .toFile(outputPath, (err, info) => {
-              if (err) throw err;
+              if (err) return rejact(err);
               if(fs.existsSync(filePath)) 
                 fs.unlinkSync(filePath);
             else {
